611451559264dd7ca9b4737e6f96f06d9e3c3306
carlosalvahan <cs.salvahan@gmail.com>
2025-02-06 16:53:43 +0800

    Setup commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InstrumentPageComponent } from './components/pages/instrument-page/instrument-page.component';
+import { OrdersPageComponent } from './components/pages/orders-page/orders-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: InstrumentPageComponent},
+    {path: 'orders', component: OrdersPageComponent},
+    {path: 'register', loadComponent: () =>  
+        import('./components/pages/register-page/register-page.component').then(r => r.RegisterPageComponent)
+    },
+    {path: '*', component: OrdersPageComponent}
+];
---
+++ src/app/components/pages/instrument-page/instrument-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instrument-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './instrument-page.component.html',
+  styleUrl: './instrument-page.component.scss'
+})
+export class InstrumentPageComponent {
+
+}
